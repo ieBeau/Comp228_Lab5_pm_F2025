f79f233d904fcb719027aff46ba4c0d1b016a319
Beau <77253127+ieBeau@users.noreply.github.com>
2025-10-29 12:24:35 -0400

    Init commit

--- Comp228_Lab5_PM_F2025/src/application/CustomGridPaneCenter.java
+++ Comp228_Lab5_PM_F2025/src/application/CustomGridPaneCenter.java
@@ -12,6 +12,9 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 class CustomGridPaneCenter extends GridPane {
     public CustomGridPaneCenter() {
@@ -30,63 +33,70 @@ class CustomGridPaneCenter extends GridPane {
 
         getColumnConstraints().addAll(col1, col2);
 
+        // Create Application Title
+        Text title = new Text("Job Application Form");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        add(title, 0, 0, 2, 1);
+        GridPane.setColumnSpan(title, 2);
+        GridPane.setHalignment(title, HPos.CENTER);
+
         // Get First Name
         Label lblFirstName = new Label("First name *");
-        add(lblFirstName, 0, 0);
+        add(lblFirstName, 0, 1);
         TextField txtFirstName = new TextField();
-        add(txtFirstName, 0, 1);
+        add(txtFirstName, 0, 2);
 
         // Get Last Name
         Label lblLastName = new Label("Last name *");
-        add(lblLastName, 1, 0);
+        add(lblLastName, 1, 1);
         TextField txtLastName = new TextField();
-        add(txtLastName, 1, 1);
+        add(txtLastName, 1, 2);
 
         // Get Email
         Label lblEmail = new Label("Email *");
-        add(lblEmail, 0, 2, 2, 1);
+        add(lblEmail, 0, 3, 2, 1);
         TextField txtEmail = new TextField();
-        add(txtEmail, 0, 3, 2, 1);
+        add(txtEmail, 0, 4, 2, 1);
 
         // Get Personal Website URL
         Label lblPersonalWebsite = new Label("Personal Website");
-        add(lblPersonalWebsite, 0, 4, 2, 1);
+        add(lblPersonalWebsite, 0, 5, 2, 1);
         TextField txtPersonalWebsite = new TextField("http://");
-        add(txtPersonalWebsite, 0, 5, 2, 1);
+        add(txtPersonalWebsite, 0, 6, 2, 1);
 
         // Get Applied Position
         Label lblPosition = new Label("Position you are applying for *");
-        add(lblPosition, 0, 6, 2, 1);
+        add(lblPosition, 0, 7, 2, 1);
         TextField txtPosition = new TextField();
-        add(txtPosition, 0, 7, 2, 1);
+        add(txtPosition, 0, 8, 2, 1);
 
         // Get Salary Requirements
         Label lblSalary = new Label("Salary requirements");
-        add(lblSalary, 0, 8);
+        add(lblSalary, 0, 9);
         TextField txtSalary = new TextField();
-        add(txtSalary, 0, 9);
+        add(txtSalary, 0, 10);
 
         // Get Start Date
         Label lblStart = new Label("When can you start?");
-        add(lblStart, 1, 8);
+        add(lblStart, 1, 9);
         TextField txtStart = new TextField();
-        add(txtStart, 1, 9);
+        add(txtStart, 1, 10);
 
         // Get Phone Number
         Label lblPhone = new Label("Phone *");
-        add(lblPhone, 0, 10);
+        add(lblPhone, 0, 11);
         TextField txtPhone = new TextField();
-        add(txtPhone, 0, 11);
+        add(txtPhone, 0, 12);
 
         // Get Fax Number
         Label lblFax = new Label("Fax");
-        add(lblFax, 1, 10);
+        add(lblFax, 1, 11);
         TextField txtFax = new TextField();
-        add(txtFax, 1, 11);
+        add(txtFax, 1, 12);
 
         // Get Relocation Status
         Label lblRelocate = new Label("Are you willing to relocate?");
-        add(lblRelocate, 0, 12, 2, 1);
+        add(lblRelocate, 0, 13, 2, 1);
         ToggleGroup tgRelocate = new ToggleGroup();
         RadioButton rbYes = new RadioButton("Yes");
         rbYes.setToggleGroup(tgRelocate);
@@ -96,25 +106,25 @@ class CustomGridPaneCenter extends GridPane {
         rbNotSure.setToggleGroup(tgRelocate);
 
         HBox hbRelocate = new HBox(10, rbYes, rbNo, rbNotSure);
-        add(hbRelocate, 0, 13, 2, 1);
+        add(hbRelocate, 0, 14, 2, 1);
 
         // Get Name of Previous Company
         Label lblPreviousCompany = new Label("Last company you worked for");
-        add(lblPreviousCompany, 0, 14, 2, 1);
+        add(lblPreviousCompany, 0, 15, 2, 1);
         TextField txtPreviousCompany = new TextField();
-        add(txtPreviousCompany, 0, 15, 2, 1);
+        add(txtPreviousCompany, 0, 16, 2, 1);
 
         // Get Reference / Comments / Questions
         Label lblComments = new Label("Reference / Comments / Questions");
-        add(lblComments, 0, 16, 2, 1);
+        add(lblComments, 0, 17, 2, 1);
         TextArea txtComments = new TextArea();
         txtComments.setWrapText(true);
         txtComments.setPrefHeight(120);
-        add(txtComments, 0, 17, 2, 4);
+        add(txtComments, 0, 18, 2, 1);
 
         // Create Submit Button
         Button bySubmit = new Button("Send Application");
-        add(bySubmit, 0, 22);
+        add(bySubmit, 0, 20);
         GridPane.setColumnSpan(bySubmit, 2);
         GridPane.setHalignment(bySubmit, HPos.CENTER);
 
